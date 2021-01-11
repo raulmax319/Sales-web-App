6372ea5faadd616bf0908b673fc7e536ba237bbe
raulmax <raulmax319@gmail.com>
2021-01-11 10:08:18 -0300

    Added Departments Views

--- Models/Department.cs
+++ Models/Department.cs
@@ -1,6 +1,6 @@
 namespace SalesWebMvc.Models {
     public class Department {
-        public int id { get; set; }
-        public string name { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
